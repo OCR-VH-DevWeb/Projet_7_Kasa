9c49cad823ca5f4459d7e696f9af26e87abdec52
VirginieH <valiendanse@gmail.com>
2022-10-24 15:03:18 +0200

    Page About / Collapse / Data / Style
    Créée /Création & Fonctionnel / Créa json pour texte Collapse / Ajustement des variables color

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,41 @@
+// import du Hook { useState }
+import React, { useState } from 'react';
+import Arrow from "../assets/Arrowcoll.png";
+import Arrowup from "../assets/ArrowUpcoll.png";
+
+
+
+
+function Collapse({category, content}) {
+  // déclare une nouvelle variable d'état en appelant useState()
+  //renvoie une paire de valeurs et initialise à null comme argument
+  //second élément renvoyé est une fonction qui permet de modifier la variable isOpen
+    const [isOpen, setIsOpen] = useState(null);
+    //change le state 
+    const stateOpen = () => setIsOpen(!isOpen);
+console.log(isOpen);
+    return (
+        <div className='wrapper'>
+            <div className='heading'>
+                <div className='title'>{category}</div>
+                <button className='dropdown' onClick={stateOpen}>
+                    <img src={isOpen ?  Arrowup : Arrow} alt="Flèche pour afficher le texte"></img>
+                </button>    
+            </div>
+            {isOpen ? (
+                 <div className='description'>{content}</div>
+            ) : 
+                null
+            }
+        </div>
+    ) 
+}
+
+export default Collapse
+
+// {collapseState ? (
+//     <div></div>
+//   ) : (
+//     <div></div>
+//   )}
+// {dataCollapse.map((data, d) => ())}
