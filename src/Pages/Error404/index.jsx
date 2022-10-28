0abf33348f496ee522791fb5428c220fcd68d9db
VirginieH <valiendanse@gmail.com>
2022-10-28 23:20:11 +0200

    Responsive page Error404 ajustement
    Fin responsive pour toutes les pages

--- src/Pages/Error404/index.jsx
+++ src/Pages/Error404/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Error404() {
   return (
     <div className="global">
-      <p className="object">404</p>
+      <h1 className="object">404</h1>
       <p className="message">Oups! La page que vous demandez n'existe pas.</p>
       <Link className="linkback" to="/">
         <p className="backhome">Retourner sur la page d'accueil</p>
@@ -15,7 +15,7 @@ function Error404() {
 export default Error404
 
 // {/* <div className="global">
-// <h1 className="object">404</h1>
+// < className="object">404</h1>
 // <h2 className="message">Oups! La page que vous demandez n'existe pas.</h2>
 // <Link className="linkback" to="/">
 // <h3 className="backhome">Retourner sur la page d'accueil</h3>
